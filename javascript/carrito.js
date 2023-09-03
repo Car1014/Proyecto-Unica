bfab1fdd921aae44ec4de5196958534f1d874412
Car1014 <carlosroo1014@gmail.com>
2023-09-02 18:27:39 -0600

    Tercera

---
+++ javascript/carrito.js
@@ -0,0 +1,29 @@
+
+
+ // Obtén el elemento de entrada y agrega un controlador de eventos clic
+ var botonRedireccion = document.getElementById("boton-compra");
+ botonRedireccion.addEventListener("click", function() {
+     // Redirige al usuario a la página deseada
+     alert("Tu pedido se realizo con exito")
+ });
+
+
+
+var botonRedireccion = document.getElementById("boton1");
+ botonRedireccion.addEventListener("click", function() {
+     // Redirige al usuario a la página deseada
+     alert("Producto agregado")
+ });
+
+
+ var botonRedireccion = document.getElementById("boton2");
+ botonRedireccion.addEventListener("click", function() {
+     // Redirige al usuario a la página deseada
+     alert("Producto agregado al carrito")
+ });
+
+ var botonRedireccion = document.getElementById("boton3");
+ botonRedireccion.addEventListener("click", function() {
+     // Redirige al usuario a la página deseada
+     alert("Producto eleminidado")
+ });
